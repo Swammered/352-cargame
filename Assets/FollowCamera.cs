7bf7808370cc9a7e228b5e1b60ffab68ccea2abd
Swammered <jamesswim12@yahoo.com>
2022-11-11 14:12:37 -0500

    better camera movement

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] GameObject thingToFollow;
-    // Update is called once per frame
-    void Update()
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Transform target;
+
+    private void Update()
     {
-        transform.position = thingToFollow.transform.position + new Vector3(0,0,-10);
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
