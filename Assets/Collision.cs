65dc53ca3ff4054739784660347402c22cadbea2
Swammered <jamesswim12@yahoo.com>
2022-11-16 11:35:31 -0500

    created packages, and pick up / drop of mechanics

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    bool hasPackage = false;
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("OUCH");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Over the square");
+
+        if(other.gameObject.tag == "Package" && !hasPackage){
+            Debug.Log("picked up package");
+            hasPackage = true;
+            Destroy(other.gameObject, 0.25f);
+        }
+
+        if(other.gameObject.tag == "Person" && hasPackage){
+            Debug.Log("package delivered");
+            hasPackage = false;
+        }
+        
     }
 }
